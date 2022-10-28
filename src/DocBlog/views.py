1eaf0fe5977d32ff19b9fc95802421b12fe6909c
Neros80 <a0415@hotmail.fr>
2022-10-28 14:53:55 +0200

    suppression de print

--- src/DocBlog/views.py
+++ src/DocBlog/views.py
@@ -5,7 +5,4 @@ from django.shortcuts import render
 def index(request):
 
     date = datetime.today()
-    print(date)
-    print(type(date))
-
     return render(request, "docBlog/index.html", context={"date": date})
